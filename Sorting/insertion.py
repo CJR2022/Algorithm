8cacd04168df1bb87d12017a4b5aad754ad001a9
CJR <wjdfbs13579@gmail.com>
2025-10-18 07:15:30 +0900

    add insertion_sort

---
+++ Sorting/insertion.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+        while j>=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+if __name__ == "__main__":
+    sample_array = [64, 25, 12, 22, 11]
+    sorted_array = insertion_sort(sample_array)
+    print("Sorted array:", sorted_array)
